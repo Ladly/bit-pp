28394da9a11a74f1b86e6023702f6e6717b4cea4
BIT Student <classroom@bgit.rs>
2018-02-08 16:55:43 +0100

    Practical 3 task 1

---
+++ functions/practical-3/task-1.js
@@ -0,0 +1,13 @@
+function insertString(stringg, stringToInput, position) {
+    newString = ""
+    for (var i = 0; i < stringg.length; i++) {
+        if (i === position) {
+            newString += stringToInput
+        } else {
+            newString += stringg[i] 
+        }
+    }
+    return newString
+}
+
+console.log(insertString("nesto normalno", "frr", 3));
