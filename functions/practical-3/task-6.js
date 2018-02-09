696bb23dea36e5bcf03fc004ae7918afcac5fe3d
BIT Student <classroom@bgit.rs>
2018-02-09 11:22:59 +0100

    P3 t6 start

---
+++ functions/practical-3/task-6.js
@@ -0,0 +1,14 @@
+// Write a function to create a specified number of elements with pre-filled numeric value array.
+
+// 6, 0 -> [0, 0, 0, 0, 0, 0]
+// 2, "none" -> ["none", "none"]
+// 2 -> [null, null] 
+
+// function fillArray(numberOfElements, element) {
+//     if(element === undefined) {
+//         element = null
+
+//     }
+//     for(var i = 0; i < )
+// }
+
