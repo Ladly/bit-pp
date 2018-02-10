fa1c16eef20da11885d88ca3d116b812efcef798
Vuk <vukivanovic.vuk@gmail.com>
2018-02-10 12:17:42 +0100

    Task 2

---
+++ functions/practical-4/task-2.js
@@ -0,0 +1,19 @@
+// Write a program that multiplies every positive element of a given array by 2.
+// Input array: [-3, 11, 5, 3.4, -8]
+// Output array: [-3, 22, 10, 6.8, -8]
+
+function multiplyEvens(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if ( arr[i] > 0 ) {
+            arr[i] *= 2;
+        } else {
+           continue;
+        }
+    }
+
+    return arr
+}
+
+input = [-3, 11, 5, 3.4, -8]
+
+console.log(multiplyEvens(input));
