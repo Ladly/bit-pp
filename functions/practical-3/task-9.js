f8eb05de9718ed9bb96bb54813d9d7bf46fd0a08
Vuk <vukivanovic.vuk@gmail.com>
2018-02-10 07:28:32 +0100

    Practical 3 task 9

---
+++ functions/practical-3/task-9.js
@@ -0,0 +1,32 @@
+// Write a function to hide email address.
+// "myemailaddress@bgit.rs" -> "myemail...@bgit.rs"
+
+function hideEmail(email) {
+    var spreadOperator = "..."
+    var hiddenEmail = ""
+    var positionOfMonkey = 0
+
+    for(var i = email.length; i >= 0; i--){
+        if(email[i] === "@") {
+            positionOfMonkey = i
+        }
+    }
+
+    for(var j = 0; j < email.length; j++) {
+        if(j < 7) {
+            hiddenEmail += email[j]
+        } else if(j === 7){
+            hiddenEmail += spreadOperator
+        } else if(j > 7 && j < positionOfMonkey) {
+            continue
+        } else if (j === positionOfMonkey) {
+            hiddenEmail += "@"
+        } else if (j > positionOfMonkey) {
+            hiddenEmail += email[j]
+        }
+    }
+
+    return hiddenEmail
+}
+
+console.log(hideEmail("myemailaddress@bgit.rs"));
